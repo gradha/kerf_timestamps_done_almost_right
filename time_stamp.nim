9f335935052df80534b5e7a18767768c301d1b29
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2016-01-25 00:49:39 +0100

    Adds timestamp parser of time parts with ns accuracy.

--- time_stamp.nim
+++ time_stamp.nim
@@ -14,6 +14,11 @@ const
   year_start = "".len
   month_start = "YYYY-".len
   days_start = "YYYY-MM-".len
+  hours_start = "YYYY-MM-DDT".len
+  minutes_start = "YYYY-MM-DDThh:".len
+  seconds_start = "YYYY-MM-DDThh:mm:".len
+  nanos_start = "YYYY-MM-DDThh:mm:ss:".len
+  max_stamp_len = "YYYY-MM-DDThh:mm:ss:012345678".len
   epoch_offset = 1970
   days_in_a_month = 30 # Because I'm worth it…
 
@@ -24,8 +29,8 @@ proc date*(x: string): Stamp =
   ## For simplicity this is just a naive implementation that will easily break
   ## under the weight of any real user input, there is no real user friendly
   ## validation here. We expect strings to be in the format YYYY-MM-DD and
-  ## that's it.
-  assert x.len == 10
+  ## that's it. Ok, maybe we allow times later too…
+  assert x.len >= 10 and x.len <= max_stamp_len
   var token: string
   do_assert 4 == x.parse_while(token, Digits, year_start)
   var
@@ -46,6 +51,45 @@ proc date*(x: string): Stamp =
   # Finally, convert the individual values to a (fake) calendar.
   result = Stamp((yyyy - epoch_offset) * year +
     (mm - 1) * days_in_a_month * day + (dd - 1) * day)
+
+  if x.len < minutes_start - 1:
+    return
+
+  # Ugh, we got some times… ok, try to parse them.
+  var hh: int
+  do_assert 2 == x.parse_while(token, Digits, hours_start)
+  do_assert 2 == token.parse_int(hh)
+  do_assert hh >= 0 and hh < 24
+  result = result + hh.h
+
+  if x.len < seconds_start - 1:
+    return
+
+  var minutes: int
+  do_assert 2 == x.parse_while(token, Digits, minutes_start)
+  do_assert 2 == token.parse_int(minutes)
+  do_assert minutes >= 0 and minutes < 60
+  result = result + minutes.i
+
+  if x.len < nanos_start - 1:
+    return
+
+  var seconds: int
+  do_assert 2 == x.parse_while(token, Digits, seconds_start)
+  do_assert 2 == token.parse_int(seconds)
+  do_assert seconds >= 0 and seconds < 60
+  result = result + seconds.s
+
+  if x.len > nanos_start:
+    var nanos: int
+    var num_nanos = x.parse_while(token, Digits, nanos_start)
+    do_assert num_nanos > 0 and num_nanos < 10
+    do_assert num_nanos == token.parse_int(nanos)
+    # Compensate lack of decimals during parsing with pow-like loop.
+    while num_nanos < 9:
+      nanos = nanos * 10
+      num_nanos.inc
+    result = result + Nano(nanos)
 
 
 proc `$`*(x: Stamp): string =
@@ -81,15 +125,20 @@ proc `$`*(x: Stamp): string =
     numeric_seconds = seconds div second
     numeric_minutes = minutes div minute
     numeric_hours = hours div hour
+    numeric_nanos = int(seconds mod second)
 
   # Return already if the time ends at midnight.
-  if numeric_seconds < 1 and numeric_minutes < 1 and numeric_hours < 1:
+  if numeric_seconds < 1 and numeric_minutes < 1 and
+      numeric_hours < 1 and numeric_nanos < 1:
     return
 
   # Aww… format an hour then.
   result &= "T" & align($numeric_hours, 2, '0') &
     ":" & align($numeric_minutes, 2, '0') &
     ":" & align($numeric_seconds, 2, '0')
+
+  if numeric_nanos > 0:
+    result &= "." & align($numeric_nanos, 9, '0')
 
 
 proc test_stamps*() =
@@ -101,3 +150,11 @@ proc test_stamps*() =
   echo "plus one month and a day is ", a + 1.m + 1.d
   echo "…plus 1h15i17s ", a + 1.m + 1.d + 1.h + 15.i + 17.s
   echo "…plus 23 hours ", a + 1.m + 2.d - 1.h
+  echo "2001.01.01T01".date
+  echo "2001.01.01T02:01".date
+  echo "2001.01.01T03:02:01".date
+  echo "2001.01.01T04:09:02.1".date
+  echo "2001.01.01T04:09:02.12".date
+  echo "2001.01.01T04:09:02.123".date
+  echo "2001.01.01T05:04:03.0123".date
+  echo "2001.01.01T06:05:04.012345678".date
