6278bd89f5e288f8f60968ee63bb5d4824226048
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2016-01-25 23:50:35 +0100

    Adds cpp stub.

--- nakefile.nim
+++ nakefile.nim
@@ -2,11 +2,18 @@ import
   bb_nake, bb_os, bb_system
 
 
+proc run_cpp() =
+  with_dir "cpp":
+    dire_shell "g++", "*.cpp", "-std=c++11", "-o", "units.exe"
+    dire_shell "./units.exe"
+
 proc run_nim() =
-  with_dir "nim": direShell nim_exe, "c", "-o:units.exe", "-r", "units"
+  with_dir "nim": dire_shell  nim_exe, "c", "-o:units.exe", "-r", "units"
 
 proc run_all() =
   run_nim()
+  run_cpp()
 
 task "nim", "Compile and run nim version": run_nim()
+task "cpp", "Compile and run cpp version": run_cpp()
 task defaultTask, "Compile all the thingies": run_all()
