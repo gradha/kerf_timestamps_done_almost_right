fe38b1ba58ff12ff74bd0b1cdc4f86af7de185c7
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2016-01-25 23:36:21 +0100

    Moves nim code to subdirectory, other languages incoming.

--- nakefile.nim
+++ nakefile.nim
@@ -1,8 +1,12 @@
 import
   bb_nake, bb_os, bb_system
 
-proc run() =
-  direShell nim_exe, "c", "-o:units.exe", "-r", "units"
 
-task defaultTask, "run":
-  run()
+proc run_nim() =
+  with_dir "nim": direShell nim_exe, "c", "-o:units.exe", "-r", "units"
+
+proc run_all() =
+  run_nim()
+
+task "nim", "Compile and run nim version": run_nim()
+task defaultTask, "Compile all the thingies": run_all()
